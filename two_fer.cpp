fd32f9288a9257aa0ab21ffd8da8036a5e12d559
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-03-04 04:00:47 +0530

    two fer

---
+++ two_fer.cpp
@@ -0,0 +1,11 @@
+#include "two_fer.h"
+
+namespace two_fer {
+
+// TODO: add your solution here
+std::string two_fer(std::string k){
+    std::string x = "One for " + k + ", one for me.";
+    return x;
+}
+}  // namespace two_fer
+
